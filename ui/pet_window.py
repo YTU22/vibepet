110edab18e0b03b7f6777dd8a3c57259723e7b6d
YTU22 <180143443@qq.com>
2026-06-02 19:00:08 +0800

    chore: bump version to v1.1.8

--- ui/pet_window.py
+++ ui/pet_window.py
@@ -20,7 +20,7 @@ from ui.tray_icon import TrayIcon
 from ui.todo_window import TodoWindow
 from core.sys_monitor import SystemMonitorThread
 
-APP_VERSION = "1.1.7"
+APP_VERSION = "1.1.8"
 
 logger = logging.getLogger("vibe_pet")
 
