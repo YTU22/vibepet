04b0f72dcd498826434586b7efc2d5c85a367c47
YTU22 <180143443@qq.com>
2026-06-02 15:52:39 +0800

    chore: exclude DEVLOG.md from git tracking

--- ui/settings_dialog.py
+++ ui/settings_dialog.py
@@ -19,7 +19,7 @@ from PyQt6.QtGui import QDesktopServices, QKeySequence, QPainter, QBrush, QPen, 
 
 from utils.helpers import resource_path, set_auto_start, is_auto_start_enabled, get_app_dir
 
-APP_VERSION = "1.1.2"
+APP_VERSION = "1.1.3"
 
 logger = logging.getLogger("vibe_pet")
 
