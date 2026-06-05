b1045c1371eb0ebbd083908a6e3080e82c55c7bf
YTU22 <180143443@qq.com>
2026-06-05 15:39:38 +0800

    release: v1.2.7 - restore original Slime character appearance

--- ui/pet_window.py
+++ ui/pet_window.py
@@ -20,7 +20,7 @@ from ui.tray_icon import TrayIcon
 from ui.todo_window import TodoWindow
 from core.sys_monitor import SystemMonitorThread
 
-APP_VERSION = "1.2.6"
+APP_VERSION = "1.2.7"
 
 logger = logging.getLogger("vibe_pet")
 
